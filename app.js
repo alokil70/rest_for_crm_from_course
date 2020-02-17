0ca7dcc238f0ee39ee7910d5a7621727bbe9ff74
Alexandr <alokil70@gmail.com>
2020-02-17 18:03:28 +0300

    upload added

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ require('./middleware/passport')(passport)
 
 
 app.use(require('morgan')('dev'))
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(require('cors')())
